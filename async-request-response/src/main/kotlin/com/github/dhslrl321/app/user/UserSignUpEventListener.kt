d575160931c27259db72155097d6e4ba7fda0a3d
dhslrl321 <dhslrl321@gmail.com>
2023-12-16 12:29:14 +0900

    commit

--- async-request-response/src/main/kotlin/com/github/dhslrl321/app/user/UserSignUpEventListener.kt
+++ async-request-response/src/main/kotlin/com/github/dhslrl321/app/user/UserSignUpEventListener.kt
@@ -4,6 +4,7 @@ import com.github.dhslrl321.app.async.transistor.AsyncOperationTransistor
 import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
+import java.lang.RuntimeException
 import kotlin.random.Random
 
 private const val SECONDS_10 = 10000L
@@ -17,16 +18,19 @@ class UserSignUpEventListener(
     @EventListener(SignUpEvent::class)
     fun listen(event: SignUpEvent) {
 
-        processComplexBusinessLogic() // blocking 10 seconds
-
-        if (Random.nextBoolean()) {
-            transistor.transitToComplete(event.operationId)
-        } else {
-            transistor.transitToFailed(event.operationId, "어떠한 사유로 인해 실패함")
+        try {
+            processComplexBusinessLogic() // blocking 10 seconds
+        } catch (e: Exception) {
+            transistor.transitToFailed(event.operationId, e.localizedMessage)
+            throw e
         }
+
+        transistor.transitToComplete(event.operationId)
     }
 
     private fun processComplexBusinessLogic() {
         Thread.sleep(SECONDS_10)
+
+        if (Random.nextBoolean()) throw RuntimeException("어떠한 사유에 의해 예외가 발생함")
     }
 }
